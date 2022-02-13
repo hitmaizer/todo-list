489ddb2c5a97e635efc2acc66ae3135583d2016c
hitmaizer <jose.sousalves@gmail.com>
2022-02-13 19:50:37 +0000

    moved state to app parent component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import Tabs from './components/Tabs';
 
 export default function App() {
+    const [toggleState, setToggleState] = React.useState(1);
+
+    function toggleTab(index) {
+        setToggleState(index);
+    };
+
+
     return (
-        <Tabs />
+        <div className="page__wrapper">
+            <Tabs 
+            clickHandler={toggleTab}
+            state={toggleState}
+            />
+        </div>
     )
 }
