236b13c1ccc1260dbe21f9a1582fe03d5878af8a
hitmaizer <jose.sousalves@gmail.com>
2022-02-14 16:15:55 +0000

    css import on app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tabs from './components/Tabs';
+import './scss/style.css';
 
 export default function App() {
     const [toggleState, setToggleState] = React.useState(1);
@@ -11,6 +12,7 @@ export default function App() {
 
     return (
         <div className="page__wrapper">
+            <h1 className="page__title">#todo</h1>
             <Tabs 
             clickHandler={toggleTab}
             state={toggleState}
