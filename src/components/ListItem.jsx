aefc083aca7b85ee8a286372070939fb2fa7dd8d
hitmaizer <jose.sousalves@gmail.com>
2022-02-14 21:52:17 +0000

    call in change function

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ListItem(props) {
     return (
         <div className="content__item flex-row">
-            <input type="checkbox" className="item__checkbox" value={props.completed} />
+            <input type="checkbox" className="item__checkbox" checked={props.completed} onChange={props.checkItem} />
             <p className="item__description">{props.task}</p>
         </div>
     )
