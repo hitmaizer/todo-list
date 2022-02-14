4ae601ae240bce49416e426f7a6be86bf73bb555
hitmaizer <jose.sousalves@gmail.com>
2022-02-14 17:38:51 +0000

    initialize states for notes and current note

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { NanoDimensions } from '@styled-icons/crypto/Nano';
 import React from 'react';
 import Tabs from './components/Tabs';
 import './scss/style.css';
+import { nanoid } from 'nanoid'
 
 export default function App() {
     const [toggleState, setToggleState] = React.useState(1);
+    const [notes, setNotes] = React.useState([])
+    const [currentNote, setCurrentNote] = React.useState({
+        id: nanoid(),
+        task: "",
+        isCompleted: false
+    })
+    
+    
 
     function toggleTab(index) {
         setToggleState(index);
