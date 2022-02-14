837c19c1c7eb6110352375f88bd9dfb54237492c
hitmaizer <jose.sousalves@gmail.com>
2022-02-14 16:58:58 +0000

    hard coded jsx markup for all tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,3 +1,4 @@
+import { DeleteBin7 } from '@styled-icons/remix-line/DeleteBin7'
 
 function Tabs(props) {
     
@@ -6,31 +7,55 @@ function Tabs(props) {
     <div className="container">
       <div className="bloc-tabs">
         <button
-          className={props.state === 1 ? "tabs active-tabs" : "tabs"}
-          onClick={() => props.clickHandler(1)}>
+        className={props.state === 1 ? "tabs active-tabs" : "tabs"}
+        onClick={() => props.clickHandler(1)}
+        >
           <span className="tab__title">
             All
           </span>
         </button>
         <button
-          className={props.state === 2 ? "tabs active-tabs" : "tabs"}
-          onClick={() => props.clickHandler(2)}>
+        className={props.state === 2 ? "tabs active-tabs" : "tabs"}
+        onClick={() => props.clickHandler(2)}
+        >
           <span className="tab__title">
             Active
           </span>
         </button>
         <button
-          className={props.state === 3 ? "tabs active-tabs" : "tabs"}
-          onClick={() => props.clickHandler(3)}>
+        className={props.state === 3 ? "tabs active-tabs" : "tabs"}
+        onClick={() => props.clickHandler(3)}
+        >
           <span className="tab__title">
             Completed
           </span>
         </button>
       </div>
-
       <div className="content-tabs">
-        <div
-          className={props.state === 1 ? "content  active-content" : "content"}>
+        <div className={props.state === 1 ? "content  active-content" : "content"}>
+          <div className="input__container flex-row">
+            <input type="text" className="alltab__input" placeholder="add details" />
+            <button className="alltab__btn">
+              <span className="btn__text">Add</span> 
+            </button>
+          </div>
+          <div className="alltab__content flex-col">            
+            <div className="content__item flex-row">
+              <input type="checkbox" className="item__checkbox" />
+              <p className="item__description">Do coding challenges</p>
+            </div>
+            <div className="content__item flex-row">
+              <input type="checkbox" className="item__checkbox" />
+              <p className="item__description">Do coding challenges</p>
+            </div>
+            <div className="content__item flex-row">
+              <input type="checkbox" className="item__checkbox" />
+              <p className="item__description">Do coding challenges</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={props.state === 2 ? "content  active-content" : "content"}>
           <div className="input__container flex-row">
             <input type="text" className="alltab__input" placeholder="add details" />
             <button className="alltab__btn">
@@ -38,7 +63,6 @@ function Tabs(props) {
             </button>
           </div>
           <div className="alltab__content flex-col">
-            
             <div className="content__item flex-row">
               <input type="checkbox" className="item__checkbox" />
               <p className="item__description">Do coding challenges</p>
@@ -51,26 +75,18 @@ function Tabs(props) {
               <input type="checkbox" className="item__checkbox" />
               <p className="item__description">Do coding challenges</p>
             </div>
-
           </div>
         </div>
 
-        <div
-          className={props.state === 2 ? "content  active-content" : "content"}>
-          <h2>Active</h2>
-          <hr />
-          <p>
-            This is going to be active items
-          </p>
-        </div>
+        <div className={props.state === 3 ? "content  active-content" : "content"}>
+        <div className="alltab__content flex-col">
+            <div className="content__item flex-row">
+              <input type="checkbox" selected className="item__checkbox" />
+              <p className="item__description item__description--completed">Do coding challenges</p>
+              <DeleteBin7 size="24px" className="item__icon" />
 
-        <div
-          className={props.state === 3 ? "content  active-content" : "content"}>
-          <h2>Completed</h2>
-          <hr />
-          <p>
-            This is going to be completed items plus delete
-          </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
