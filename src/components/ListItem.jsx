a0794a58d8595ff0170083c6a9076bea553ff1be
hitmaizer <jose.sousalves@gmail.com>
2022-02-14 18:38:13 +0000

    dynamic rendering of items

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function ListItem() {
+export default function ListItem(props) {
     return (
         <div className="content__item flex-row">
-            <input type="checkbox" className="item__checkbox" />
-            <p className="item__description">Do coding challenges</p>
+            <input type="checkbox" className="item__checkbox" value={props.completed} />
+            <p className="item__description">{props.task}</p>
         </div>
     )
 }
