1dc6cc52eda9d4624b502a66a2f979b527f41df0
hitmaizer <jose.sousalves@gmail.com>
2022-02-14 18:00:15 +0000

    currentNote logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import Tabs from './components/Tabs';
 import './scss/style.css';
 import { nanoid } from 'nanoid'
-import { North } from 'styled-icons/material';
+
 
 export default function App() {
     const [toggleState, setToggleState] = React.useState(1);
     const [notes, setNotes] = React.useState([])
     const [currentNote, setCurrentNote] = React.useState({
-        id: nanoid(),
         task: "",
         isCompleted: false
     })
     
     function handleChange(e) {
         const typingTask = e.target.value
-        setCurrentNote({...currentNote, task: typingTask})
+        setCurrentNote({...currentNote, [e.target.name]: typingTask, id: nanoid()})
+
     }
 
     console.log(currentNote)
