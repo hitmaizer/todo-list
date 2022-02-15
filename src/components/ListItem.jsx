f5852a2dda9d21938127bfe400b596166cc4ee92
hitmaizer <jose.sousalves@gmail.com>
2022-02-15 22:19:33 +0000

    fixed onclick

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ListItem(props) {
     return (
         <div className="content__item flex-row">
-            <input type="checkbox" className="item__checkbox" defaultChecked={props.completed} onClick={props.checkItem} />
+            <input type="checkbox" className="item__checkbox" defaultChecked={props.completed} onClick={props.handleClick}/>
             
             <p className="item__description">{props.task}</p>
         </div>
