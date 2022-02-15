f9c5b5c9a853d60f7dd57ad133650d5a5072ba03
hitmaizer <jose.sousalves@gmail.com>
2022-02-15 20:03:14 +0000

    defaultchecked on checkbox component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function ListItem(props) {
     return (
         <div className="content__item flex-row">
-            <input type="checkbox" className="item__checkbox" checked={props.completed} onChange={props.checkItem} />
+            <input type="checkbox" className="item__checkbox" defaultChecked={props.completed} onClick={props.checkItem} />
+            
             <p className="item__description">{props.task}</p>
         </div>
     )
